9a671d8705d26a729cb51fbd0102ab189880fe67
lph19922010 <2217953908@qq.com>
2019-06-25 21:45:24 +0800

    税率计算器

--- calculator.py
+++ calculator.py
@@ -5,15 +5,15 @@ import sys
 import csv
 import collections
 
-tax_rate_line = collections.nametuple('tax_rate_table', [start_p, ratio, deduction])
+tax_rate_line = collections.namedtuple('tax_rate_line', ['start_p', 'ratio','deduction'])
 
 tax_rate_table = [
-                    tax_rate_line(80000, 0.45, 15160)
-                    tax_rate_line(55000, 0.35, 7160)
-                    tax_rate_line(35000, 0.3, 4410)
-                    tax_rate_line(25000, 0.25, 2660)
-                    tax_rate_line(12000, 0.2, 1410)
-                    tax_rate_line(3000, 0.1, 210)
+                    tax_rate_line(80000, 0.45, 15160),
+                    tax_rate_line(55000, 0.35, 7160),
+                    tax_rate_line(35000, 0.3, 4410),
+                    tax_rate_line(25000, 0.25, 2660),
+                    tax_rate_line(12000, 0.2, 1410),
+                    tax_rate_line(3000, 0.1, 210),
                     tax_rate_line(0, 0.03, 0)
                     ]
 tax_threshold = 5000
@@ -45,18 +45,29 @@ def social_payment(inco, soc_f):                         #社会保险费用计算
     for key,value in soc_f.items():
         soc_f[key] = float(value)
 
-    soc_ratio = sum(soc_f[YangLao], soc_f[YiLiao], soc_f[ShiYe], soc_f[GongShang], soc_f[ShengYu], soc_f[GongJiJin])
+    soc_ratio = soc_f['YangLao']+ soc_f['YiLiao']+ soc_f['ShiYe']+ soc_f['GongShang']+ soc_f['ShengYu']+ soc_f['GongJiJin']
 
-    if inco < soc_f[JiShuiL]:
-        soc_payment = soc_f[JiShuiL] * soc_ratio
-    elif inco < soc_f[JiShuiH]:
+    if inco < soc_f['JiShuL']:
+        soc_payment = soc_f['JiShuL'] * soc_ratio
+    elif inco < soc_f['JiShuH']:
         soc_payment = inco * soc_ratio
     else:
-        soc_payment = soc_f[JiShuiH] * soc_ratio
+        soc_payment = soc_f['JiShuH'] * soc_ratio
     return soc_payment
 
+def l_str(list_t):
+    str_s = ''
+    j = 1
+    for i in list_t:
+        if j < len(list_t):
+            str_s = str_s + str(i) +','
+        else:
+            str_s += str(i)
+        j += 1
+    return str_s
+
 def tax_compute(salary_f, social_f):
-    count = 0
+    salary_table = []
     for key,value in salary_f.items():
         try:
             income = int(value)
@@ -66,17 +77,23 @@ def tax_compute(salary_f, social_f):
         soc_payment = social_payment(income, social_f)
         tax_payable = income - soc_payment - tax_threshold 
         for tax_rat_l in tax_rate_table:
-            if tax_payable > tax_rat_l[start_p]:
-                tax = tax_paysble * tax_rat_l[ratio] - tax_rat[deduction]
+            
+            if tax_payable > tax_rat_l.start_p:
+                tax = tax_payable * tax_rat_l.ratio - tax_rat_l.deduction
             else:
                 tax = 0
             after_tax_salary = income -soc_payment - tax
-        salary_l = [key,income,soc_paymet,tax,after_tax_salary]
-        salary_table[count] = salary_l
-        count += 1
+        salary_l = l_str([key, income,soc_payment,tax,after_tax_salary])
+        salary_table.append(salary_l)
     return salary_table
+
+def outfile(l_f):
+    with open('taxtable.csv', 'w') as f:
+        for l in l_f:
+            f.write(l+'\n')
 
 if __name__ == '__main__':
     salary_file = get_salary_file(sys.argv[1])         #获取员工工资数据文件
     social_file = get_socialsecurity_file(sys.argv[2]) #获取社保比例配置文件
-    tax_compute(salary_file, social_file)              #应纳税计算
+    salary_tabf = tax_compute(salary_file, social_file) #应纳税计算
+    outfile(salary_tabf)
