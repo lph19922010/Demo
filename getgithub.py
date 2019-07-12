e8ffa1ec844779cf04d9263c657fa5f1166cd0af
lph19922010 <2217953908@qq.com>
2019-07-12 12:10:42 +0800

    实验楼github仓库爬取

--- getgithub.py
+++ getgithub.py
@@ -1,20 +1,19 @@
 #!usr/bin/env python3
 # _*_ coding:utf-8 _*_
 
-import scapy
+import scrapy
 
-class shiyanlourepositoritySpider(scrapy.Spider):
+class ShiyanlourepositoritySpider(scrapy.Spider):
     name = 'shiyanlou_repositority'
     
-    @property
-    def start_urls(self):
-        url_list = ['https://github.com/shiyanlou?tab=repositories']
-        return url_list
+    def start_requests(self):
+        url = 'https://github.com/shiyanlou\?tab\=repositories'
+        yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        for course  in response.css('div.user-repositories-list'):
+        for repositority in response.css('div#user-repositories-list'):
             yield {
-                "name":course.css('h3 a::text').extract().strip()
-                "update_time":course.css('div.mt-2 relative-time::attr(datetime)').extract().strip()
+                "name":repositority.css('h3 a::text').extract().strip(),
+                "update_time":repositority.css('div.f6.text-gray.mt-2 relative-time::attr(datetime)').extract().strip()
                     }
 
